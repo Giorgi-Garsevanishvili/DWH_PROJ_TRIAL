7116f74aefb3acd3e7862af0262cf2e63efec843
Giorgi-Garsevanishvili <george.garsevanidi@gmail.com>
2025-09-16 11:50:35 +0300

    Update stored_proc_load_bronze.sql

--- scripts/bronze/stored_proc_load_bronze.sql
+++ scripts/bronze/stored_proc_load_bronze.sql
@@ -1,8 +1,120 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME
 	BEGIN TRY
-		PRINT 'EXECUTION IN PROGRESS';
-		PRINT 'NOT FULFILLED...';
+		SET @batch_start_time = GETDATE();
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+		PRINT 'Loading Bronze Layer...';
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+		PRINT 'Loading CRM Tables...'
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+
+		PRINT '-----------------------------------------------'
+		SET @start_time = GETDATE()
+
+		PRINT '>>> Truncating Table:  bronze.crm_cust_info'
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>>> Inserting Data Into: bronze.crm_cust_info'
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		)
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duration: ' + CAST(DATEDIFF(second, @start_time,@end_time)AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------'
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.crm_prd_info'
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>>> Inserting Data Into: bronze.crm_prd_info'
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		)
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------'
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.crm_sales_details'
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>>> Inserting Data Into: bronze.crm_sales_details'
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		)
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------'
+
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
+		PRINT 'Loading ERP Tables...'
+		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.erp_cust_az12'
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>>> Inserting Data Into: bronze.erp_cust_az12'
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+		 FIRSTROW = 2,
+		 FIELDTERMINATOR = ',',
+		 TABLOCK
+		)
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duartion: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------'
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.erp_local_a101'
+		TRUNCATE TABLE bronze.erp_local_a101;
+		PRINT '>>> Inserting Data Into: bronze.erp_local_a101'
+		BULK INSERT bronze.erp_local_a101
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		)
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------'
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.erp_px_cat_g1v2'
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>>> Inserting Data Into: bronze.erp_px_cat_g1v2'
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\vipgi\Desktop\SQL Course\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		)
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Total Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+
+		PRINT '------------------------------------------------'
+		SET @batch_end_time = GETDATE();
+		PRINT 'Bronze Layer Loading is Completed'
+		PRINT '  - Total Duration: ' + CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) AS VARCHAR) + ' Seconds' 
 	END TRY
 	BEGIN CATCH
 		PRINT '=========================================='
