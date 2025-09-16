a5a249d3ad9927d9168868907f92edb2d7ee1257
Giorgi-Garsevanishvili <george.garsevanidi@gmail.com>
2025-09-16 20:18:56 +0300

    Update stored_proc_load_silver.sql

--- scripts/silver/stored_proc_load_silver.sql
+++ scripts/silver/stored_proc_load_silver.sql
@@ -78,6 +78,45 @@ BEGIN
 		PRINT '------------------------------------------------------'
 
 		PRINT '------------------------------------------------------'
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: bronze.crm_prd_info'
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>>> Inserting Data Into: bronze.crm_prd_info '
+		
+
+		INSERT INTO silver.crm_prd_info (
+		   prd_id,
+		   cat_id,
+           prd_key,
+           prd_nm,
+           prd_cost,
+           prd_line,
+           prd_start_dt,
+           prd_end_dt
+		   )
+		SELECT prd_id,
+		   REPLACE(SUBSTRING(prd_key, 1, 5 ), '-', '_') AS cat_id,
+		   SUBSTRING(prd_key,7, LEN(prd_key)) AS prd_key,
+		   TRIM(prd_nm) AS prd_nm,
+		   ISNULL(prd_cost, 0) AS prd_cost,  
+		   CASE UPPER(TRIM(prd_line))
+			WHEN 'R' THEN 'Road'
+			WHEN 'M' THEN 'Mountain'
+			WHEN 'S' THEN 'Other Sales'
+			WHEN 'T' THEN 'Touring'
+			ELSE 'n/a'
+		   END AS prd_line,
+		   CAST(prd_start_dt AS DATE) AS prd_start_dt,
+		   CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS DATE
+			) AS prd_end_dt
+		FROM bronze.crm_prd_info
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed: Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------------'
+
+		PRINT '------------------------------------------------------'
 		SET @batch_end_time = GETDATE()
 		PRINT '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
 		PRINT 'Loading Silver Layer is Completed'
