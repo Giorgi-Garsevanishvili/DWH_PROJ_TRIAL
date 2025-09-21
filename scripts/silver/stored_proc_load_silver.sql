b2aa090273d9a2fe0bfc41018384ed32061e87a4
Giorgi-Garsevanishvili <george.garsevanidi@gmail.com>
2025-09-21 18:53:37 +0300

    Update stored_proc_load_silver.sql

--- scripts/silver/stored_proc_load_silver.sql
+++ scripts/silver/stored_proc_load_silver.sql
@@ -18,6 +18,7 @@ Usage Example:
 ===============================================================================
 */
 
+
 CREATE OR ALTER PROCEDURE silver.load_silver AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
@@ -79,9 +80,9 @@ BEGIN
 
 		PRINT '------------------------------------------------------'
 		SET @start_time = GETDATE()
-		PRINT '>>> Truncating Table: bronze.crm_prd_info'
-		TRUNCATE TABLE bronze.crm_prd_info;
-		PRINT '>>> Inserting Data Into: bronze.crm_prd_info '
+		PRINT '>>> Truncating Table: silver.crm_prd_info'
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>>> Inserting Data Into: silver.crm_prd_info '
 		
 
 		INSERT INTO silver.crm_prd_info (
@@ -114,6 +115,143 @@ BEGIN
 
 		SET @end_time = GETDATE()
 		PRINT 'Insert Completed: Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------------'
+
+		PRINT '------------------------------------------------------'
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: silver.crm_sales_details'
+		TRUNCATE TABLE silver.crm_sales_details;
+		PRINT '>>> Inserting Data into: silver.crm_sales_details'
+
+		INSERT INTO silver.crm_sales_details (
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		sls_order_dt,
+		sls_ship_dt,
+		sls_due_dt,
+		sls_sales,
+		sls_quantity,
+		sls_price
+		)
+		SELECT 
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		CASE 
+		 WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) 
+		END AS sls_order_dt,
+		CASE 
+		 WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) 
+		END AS sls_ship_dt,
+		CASE 
+		 WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) 
+		END AS sls_due_dt,
+		CASE 
+		 WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
+		 ELSE sls_sales
+		END AS sls_sales,
+		sls_quantity,
+		CASE 
+			WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0)
+			ELSE sls_price
+		END AS sls_price
+		FROM bronze.crm_sales_details
+		
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------------'
+
+		PRINT '----------------------------------------'
+		PRINT 'Loading ERP Tables'
+		PRINT '----------------------------------------'
+
+		PRINT '------------------------------------------------------'
+
+		SET @start_time = GETDATE()
+		PRINT '>>> Truncating Table: silver.erp_cust_az12'
+		TRUNCATE TABLE silver.erp_cust_az12;
+		PRINT '>>> Inserting Data Into: silver.erp_cust_az12'
+		INSERT INTO silver.erp_cust_az12 (
+		cid,
+		bdate,
+		gen
+		)
+		SELECT 
+		 CASE 
+			 WHEN cid like 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+			 ELSE cid
+		 END AS cid,
+		 CASE
+			WHEN bdate >= GETDATE() THEN NULL
+			WHEN bdate <= '1924-01-01' THEN NULL
+			ELSE bdate
+		 END AS bday,
+		 CASE 
+			WHEN UPPER(TRIM(gen)) IN ('F' , 'FEMALE') THEN 'Female'
+			WHEN UPPER(TRIM(gen)) IN ('M' , 'MALE') THEN 'Male'
+			ELSE 'n/a'
+		 END AS gen
+		FROM bronze.erp_cust_az12
+
+		SET @end_time = GETDATE()
+		PRINT 'Insert Completed. Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------------'
+
+		PRINT '------------------------------------------------------'
+
+		SET @start_time = GETDATE()
+
+		PRINT '>>> Truncating Table: silver.erp_local_a101'
+		TRUNCATE TABLE silver.erp_local_a101
+		PRINT '>>> Inserting Data Into: silver.erp_local_a101'
+
+		INSERT INTO silver.erp_local_a101 (
+		cid,
+		cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid,
+			CASE  
+			  WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+			  WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+			  WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+			  ELSE TRIM(cntry)
+			END AS cntry
+		FROM bronze.erp_local_a101
+		
+		SET @end_time = GETDATE()
+
+		PRINT 'Insert Completed. Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
+		PRINT '------------------------------------------------------'
+
+		PRINT '------------------------------------------------------'
+
+		SET @start_time = GETDATE()
+
+		PRINT '>>> Truncating Table: silver.erp_px_cat_g1v2'
+		TRUNCATE TABLE silver.erp_px_cat_g1v2
+		PRINT '>>> Inserting Data Into: silver.erp_px_cat_g1v2'
+
+		INSERT INTO silver.erp_px_cat_g1v2 (
+		id,
+		cat,
+		subcat,
+		maintenance
+		)
+		SELECT 
+			TRIM(id),
+			TRIM(cat),
+			TRIM(subcat),
+			TRIM(maintenance)
+		FROM bronze.erp_px_cat_g1v2
+
+		SET @end_time = GETDATE()
+
+		PRINT 'Insert Completed. Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' Seconds'
 		PRINT '------------------------------------------------------'
 
 		PRINT '------------------------------------------------------'
